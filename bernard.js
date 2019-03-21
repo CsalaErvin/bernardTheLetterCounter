4b727da084bf2ea05764fe5c0e2b6839debc0887
Ervin Csala <csala.ervin@gmail.com>
2019-03-21 23:39:08 +0100

    firstVersion

--- bernard.js
+++ bernard.js
@@ -1,6 +1,15 @@
-var sentence = prompt('Please enter a sentence in the box:')
-var character = prompt('Please enter the character to be counted:')
-for (var counter = 0; i < 3; i++) {
+function bernardTheLetterCounter () {
+	var sentence = prompt('Please enter a sentence in the box:');
+	var character = prompt('Please enter the character to be counted:');
+	var characterCount = 0;
+	for (counter = 0; counter <= sentence.length; counter++) {
+		if (sentence.charAt(counter) == character) {
+			characterCount++;
+		};
+	};
+	
+	alert('The letter ' + character + ' occurs ' + characterCount + ' times in this sentence.');
+	return;
+};
 
-}
-alert('The letter X occurs Y times in this sentence.')
+bernardTheLetterCounter();
