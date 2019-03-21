767065abecf3dce40f0b3cb3db9d793060310792
Ervin Csala <csala.ervin@gmail.com>
2019-03-21 22:58:42 +0100

    basicStructure

---
+++ bernard.js
@@ -0,0 +1,6 @@
+var sentence = prompt('Please enter a sentence in the box:')
+var character = prompt('Please enter the character to be counted:')
+for (var counter = 0; i < 3; i++) {
+
+}
+alert('The letter X occurs Y times in this sentence.')
